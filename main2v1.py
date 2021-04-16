8be18ff0dab1649c00c6b5aedbe00a16d0703193
gameleiro <74730436+gameleiro@users.noreply.github.com>
2021-04-16 15:00:49 -0300

    Add files via upload

--- main2v1.py
+++ main2v1.py
@@ -2,9 +2,8 @@
 import streamlit as st
 
 
+def if __name__ == '__main__':
 
-
-with st.beta_expander("Sobre", expanded=True):
     st.write(""" 
     Idealizada pelos Peritos Criminais Federais: **Igor Gameleiro**, **Rafaela da Fonte** e **Vitor Gomes**, a ferramenta busca auxiliar na análise de sobrepreço a partir de técnicas de visualização de dados e de estimação não paramétrica. 
     """)
